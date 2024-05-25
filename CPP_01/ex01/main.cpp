1dec37e89c0cd46d639f5f0bf6b44fb419d579ea
alineagq <alinequeirozagq@gmail.com>
2024-05-25 19:32:22 -0300

    fix: correct program name.

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -6,7 +6,6 @@ int main() {
     int N = 5;
     std::string name = "HordeZombie";
 
-    // Create a horde of N zombies
     Zombie* horde = zombieHorde(N, name);
     if (horde != NULL) {
         for (int i = 0; i < N; ++i) {
