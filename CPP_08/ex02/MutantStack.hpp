eb48633cb0e15d33e41feb56b52d0094aaa2400d
Aline Queiroz <89541648+alineagq@users.noreply.github.com>
2024-11-06 01:13:34 +0000

    Refactor MutantStack and main.cpp to improve iterator support and enhance output clarity

--- CPP_08/ex02/MutantStack.hpp
+++ CPP_08/ex02/MutantStack.hpp
@@ -7,12 +7,11 @@
 template<typename T>
 class MutantStack : public std::stack<T> {
 public:
-    using std::stack<T>::stack; // inherit constructors
-
-    // Iterator support
+    // Typedefs for iterators
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
+    // Iterator support functions
     iterator begin() { return std::stack<T>::c.begin(); }
     iterator end() { return std::stack<T>::c.end(); }
 
--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -4,9 +4,10 @@
 int main() {
     MutantStack<int> mstack;
 
+    // Testing basic stack operations
     mstack.push(5);
     mstack.push(17);
-    std::cout << "Top: " << mstack.top() << std::endl;
+    std::cout << "Top element: " << mstack.top() << std::endl;
 
     mstack.pop();
     std::cout << "Size after pop: " << mstack.size() << std::endl;
@@ -14,20 +15,22 @@ int main() {
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
+    mstack.push(0);
 
-    // Output elements using iterators
+    // Testing iterator functionality
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
 
+    std::cout << "Stack contents:" << std::endl;
     while (it != ite) {
         std::cout << *it << std::endl;
         ++it;
     }
 
-    // Copy to std::stack and output using top() and pop()
+    // Copying to std::stack
     std::stack<int> s(mstack);
-    std::cout << "Top of std::stack: " << s.top() << std::endl;
-    s.pop();
 
     return 0;
 }
