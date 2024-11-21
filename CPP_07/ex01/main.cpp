c16415baa44c4dc9d340fc86d626ab2df80a2bba
alineagq <alinequeirozagq@gmail.com>
2024-11-21 19:42:05 -0300

    Refactor main.cpp to improve code readability and add new functionality

--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -1,34 +1,51 @@
 #include <iostream>
 #include "iter.hpp"
 
-class Awesome
-{
-  public:
-    Awesome( void ) : _n( 42 ) { return; }
-    int get( void ) const { return this->_n; }
-  private:
-    int _n;
-};
-
-std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
-{
-  o << rhs.get();
-  return o;
+void printInt(int n) {
+    std::cout << n << " ";
 }
 
-template< typename T >
-void print( T& x )
-{
-  std::cout << x << std::endl;
-  return;
+void printDouble(double d) {
+    std::cout << d << " ";
+}
+
+template <typename T>
+void multiplyByTwo(T& value) {
+    value = value * 2;
+}
+
+template <typename T>
+void printElement(const T& elem) {
+    std::cout << elem << " ";
 }
 
 int main() {
-  int tab[] = { 0, 1, 2, 3, 4 };
-  Awesome tab2[5];
+    std::cout << "Test 1 - Printing integers: ";
+    int intArr[] = {1, 2, 3, 4, 5};
+    iter(intArr, 5, printInt);
+    std::cout << std::endl;
 
-  iter( tab, 5, print<const int> );
-  iter( tab2, 5, print<Awesome> );
+    std::cout << "Test 2 - Printing doubles: ";
+    double doubleArr[] = {1.1, 2.2, 3.3, 4.4};
+    iter(doubleArr, 4, printDouble);
+    std::cout << std::endl;
 
-  return 0;
+    std::cout << "Test 3 - Multiplying by 2 (integers): ";
+    int intArr2[] = {1, 2, 3, 4};
+    iter(intArr2, 4, multiplyByTwo<int>);
+    iter(intArr2, 4, printInt);
+    std::cout << std::endl;
+
+    std::cout << "Test 4 - Multiplying by 2 (doubles): ";
+    double doubleArr2[] = {1.1, 2.2, 3.3};
+    iter(doubleArr2, 3, multiplyByTwo<double>);
+    iter(doubleArr2, 3, printDouble);
+    std::cout << std::endl;
+
+    std::cout << "Test 5 - Printing elements of a string array: ";
+    const char* strArr[] = {"Hello", "World", "C++"};
+    iter<const char*>(strArr, 3, printElement<const char*>);
+    std::cout << std::endl;
+
+    return 0;
 }
